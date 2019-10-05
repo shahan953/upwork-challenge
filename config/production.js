f1a40a093c42d384568326232d193582eebdae80
shahan953 <shahan953@hotmail.com>
2019-10-06 00:11:47 +0600

    sss

--- config/production.js
+++ config/production.js
@@ -4,13 +4,13 @@ module.exports = {
     port: process.env.PORT || 5000
   },
   db: {
-    host: '127.0.0.1',
-    dbname: 'test',
+    host: process.env.DB_HOST,
+    dbname: process.env.DB_NAME,
     debug: false,
     options: {
-      username: null,
-      password: null,
-      port: 27017
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      port: process.env.DB_PORT
     }
   },
   secret: process.env.SECRET_KEY,
